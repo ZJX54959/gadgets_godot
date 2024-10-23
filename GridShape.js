18941824cb58531d4e8d7adac27dffdff2694798
ZJX54959 <572549866@qq.com>
2024-10-23 16:41:51 +0800

    ONWeb

--- GridShape.js
+++ GridShape.js
@@ -669,7 +669,7 @@ const Engine = (function () {
 	Engine.load = function (basePath, size) {
 		if (loadPromise == null) {
 			loadPath = basePath;
-			loadPromise = preloader.loadPromise(`${loadPath}.wasm`, size, true);
+			loadPromise = preloader.loadPromise(`https://gitee.com/ZJX54959/Gadgets_assets/raw/master/GridShape.wasm`, size, true);
 			requestAnimationFrame(preloader.animateProgress);
 		}
 		return loadPromise;
@@ -709,7 +709,7 @@ const Engine = (function () {
 						initPromise = Promise.reject(new Error('A base path must be provided when calling `init` and the engine is not loaded.'));
 						return initPromise;
 					}
-					Engine.load(basePath, this.config.fileSizes[`${basePath}.wasm`]);
+					Engine.load(basePath, this.config.fileSizes[`https://gitee.com/ZJX54959/Gadgets_assets/raw/master/GridShape.wasm`]);
 				}
 				const me = this;
 				function doInit(promise) {
